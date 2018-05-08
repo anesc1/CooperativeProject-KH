bc5aee44a1af44dc8bd87305b999c51755dac90a
anesc1 <anesc1@naver.com>
2018-05-08 22:15:03 +0900

    phpadd

--- app/src/main/java/com/example/gogoooma/cooperativeproject/FirstActivity.java
+++ app/src/main/java/com/example/gogoooma/cooperativeproject/FirstActivity.java
@@ -1,15 +1,15 @@
 package com.example.gogoooma.cooperativeproject;
 
 import android.app.Fragment;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class FirstActivity extends Fragment {
     View v;
+
 
     @Nullable
     @Override
--- app/src/main/java/com/example/gogoooma/cooperativeproject/MainActivity.java
+++ app/src/main/java/com/example/gogoooma/cooperativeproject/MainActivity.java
@@ -1,21 +1,19 @@
 package com.example.gogoooma.cooperativeproject;
 
-import android.app.Activity;
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.ActionBar;
-import android.text.Layout;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import java.util.List;
 
@@ -60,7 +58,7 @@ public class MainActivity extends AppCompatActivity
         boolean admin = false;
         List<String> team = null;
 
-        Member user = new Member(name, age, phoneNum, password, admin, team);
+//        Member user = new Member(name, age, phoneNum, password, admin, team);
     }
 
     @Override
