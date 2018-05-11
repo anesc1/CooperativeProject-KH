c720f488b9f4e958e5b211192c87f77d04ed3303
RHEE <andy950605@gmail.com>
2018-05-11 19:06:27 +0900

    test

--- app/src/main/java/com/example/gogoooma/cooperativeproject/CallData.java
+++ app/src/main/java/com/example/gogoooma/cooperativeproject/CallData.java
@@ -17,6 +17,7 @@ import java.util.StringTokenizer;
  * Created by anesc on 2018-04-06.
  */
 
+// 등희 어깨 듬직보스
 public class CallData extends AppCompatActivity {
     TextView getData;
     phpDown get;
