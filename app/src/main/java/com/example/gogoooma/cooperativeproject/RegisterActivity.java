0f23e186743b9d29dcfc5841e4f8bd5c00c115a9
anesc1 <anesc1@naver.com>
2018-05-16 23:53:32 +0900

    RegisterActivity fin

--- app/src/main/java/com/example/gogoooma/cooperativeproject/RegisterActivity.java
+++ app/src/main/java/com/example/gogoooma/cooperativeproject/RegisterActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -15,16 +16,23 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class RegisterActivity extends AppCompatActivity {
+    CallData callData = new CallData("http://anesc1.cafe24.com/memberdown.php");
+    ArrayList<String> phoneNumList;
+    int num_mem;
+    String Name,phoneNum,pass,passconf,age;
     EditText edit_Name;
     EditText edit_phoneNum;
     EditText edit_pass;
     EditText edit_age;
+    EditText edit_passconf;
     Button regbtn;
     RegisterData insert;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
@@ -32,20 +40,52 @@ public class RegisterActivity extends AppCompatActivity {
         edit_phoneNum = (EditText)findViewById(R.id.editText6);
         edit_pass = (EditText)findViewById(R.id.editText7);
         edit_age = (EditText)findViewById(R.id.editText4);
+        edit_passconf = (EditText)findViewById(R.id.editText8);
+        phoneNumList = new ArrayList<>();
+
 
     }
 
     public void onRegister(View view) {
-        String Name = edit_Name.getText().toString();
-        String phoneNum = edit_phoneNum.getText().toString();
-        String pass = edit_pass.getText().toString();
-        String age = edit_age.getText().toString();
+         Name = edit_Name.getText().toString();
+         phoneNum = edit_phoneNum.getText().toString();
+         pass = edit_pass.getText().toString();
+         passconf = edit_passconf.getText().toString();
+         age = edit_age.getText().toString();
 
-        insert = new RegisterData();
-        insert.execute(Name,phoneNum,pass,age);
-        Intent intent = new Intent();
-        setResult(1, intent);
-        finish();
+        num_mem = callData.arr.size()/4;
+        for(int i =0; i<num_mem;i++)
+        {
+            String temp = callData.arr.get(i*4 +1);
+            phoneNumList.add(temp);
+        }
+        Toast.makeText(RegisterActivity.this,phoneNumList.toString(),Toast.LENGTH_SHORT).show();
+
+
+            for (int i = 0; i < num_mem; i++) {
+                if (phoneNum.equals(phoneNumList.get(i)))
+                {
+                    Toast.makeText(RegisterActivity.this, "이미 존재하는 번호입니다.", Toast.LENGTH_SHORT).show();
+                }
+            }
+            
+            if (pass.equals(passconf)) {
+                insert = new RegisterData();
+                insert.execute(Name, phoneNum, pass, age);
+                Intent intent = new Intent();
+                setResult(1, intent);
+                finish();
+            } else {
+                edit_Name.setText(Name);
+                edit_phoneNum.setText(phoneNum);
+                edit_pass.setText(pass);
+                edit_passconf.setText("");
+                edit_age.setText(age);
+                Toast.makeText(RegisterActivity.this, "비밀번호와 비밀번호확인이 다릅니다", Toast.LENGTH_SHORT).show();
+                edit_passconf.requestFocus();
+            }
+
+
     }
 
     class RegisterData extends AsyncTask<String, Void, String> {
@@ -71,13 +111,13 @@ public class RegisterActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(String... params) {
 
-            String name= (String)params[0];
-            String phoneNum = (String)params[1];
-            String password = (String)params[2];
-            String age = (String)params[3];
+            String phpname= (String)params[0];
+            String phpphoneNum = (String)params[1];
+            String phppassword = (String)params[2];
+            String phpage = (String)params[3];
 
             String serverURL = "http://anesc1.cafe24.com/memberup.php";
-            String postParameters = "&name=" + name + "&phoneNum=" + phoneNum + "&password=" + password + "&age=" + age;
+            String postParameters = "&name=" + phpname + "&phoneNum=" + phpphoneNum + "&password=" + phppassword + "&age=" + phpage;
 
 
             try {
