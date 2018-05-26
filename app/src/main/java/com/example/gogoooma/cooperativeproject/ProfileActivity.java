c1e43215907ce6ba79f08bbe854d71795a50aede
Gogoooma <sinko06@naver.com>
2018-05-26 23:31:27 +0900

    invitation

--- app/src/main/java/com/example/gogoooma/cooperativeproject/ProfileActivity.java
+++ app/src/main/java/com/example/gogoooma/cooperativeproject/ProfileActivity.java
@@ -21,6 +21,7 @@ public class ProfileActivity extends AppCompatActivity {
     Member member;
     boolean flag;
     RegisterTeam insert;
+    DeleteTeam delete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,24 +49,20 @@ public class ProfileActivity extends AppCompatActivity {
 
 
     public void onInvite(View view) {
-        // 현재 팀 받아오기
         Team team = GlobalVariable.g_nowTeam;
-        // 새로운 멤버의 폰넘버 member 변수가 새 멤버
         String newMember = member.getPhoneNum();
-        // 이거 insert 모르겠다
         insert = new RegisterTeam();
         String teamMember = "";
-        // teamMember에 팀에 있는 멤버랑 새로운 멤버 폰넘버 /로 구분해서 저장
         for(int i=0; i<team.getMembers().size(); i++){
             teamMember = teamMember + team.getMembers().get(i).getPhoneNum()+"/";
         }
         teamMember = teamMember + newMember;
-        // 토스트에서는 잘 들어온 거 확인
         Toast.makeText(this, teamMember, Toast.LENGTH_SHORT).show();
-        //근데 여기 안 들어가
+        delete = new DeleteTeam();
+        String num = String.valueOf(team.getTeamNum());
+        delete.execute(num);
         insert.execute(team.getTeamName(),
-                String.valueOf(3), "", "", teamMember);
-        // 여기도 잘 저장돼서 팀 리스트에는 잘 들어가
+                num, "", "", teamMember);
         GlobalVariable.g_nowTeam.members.add(member);
         GlobalVariable.g_team.get(team.getTeamNum()).members.add(member);
         finish();
@@ -102,6 +99,89 @@ public class ProfileActivity extends AppCompatActivity {
 
             String serverURL = "http://anesc1.cafe24.com/teamup.php";
             String postParameters = "&teamName=" + phpteamName + "&teamNum=" + phpteamNum + "&leader=" + phpleader + "&admin=" + phpadmin + "&member=" + phpmember;
+
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                //httpURLConnection.setRequestProperty("content-type", "application/json");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+
+                InputStream inputStream;
+                if (responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                } else {
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    sb.append(line);
+                }
+
+
+                bufferedReader.close();
+
+
+                return sb.toString();
+
+
+            } catch (Exception e) {
+
+                return new String("Error: " + e.getMessage());
+            }
+
+        }
+    }
+    class DeleteTeam extends AsyncTask<String, Void, String> {
+        ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(ProfileActivity.this,
+                    "Please Wait", null, true, true);
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String phpteamNum1 = (String) params[0];
+
+            String serverURL = "http://anesc1.cafe24.com/teamup1.php";
+            String postParameters = "&teamNum=" + phpteamNum1;
 
 
             try {
